cc76f8438a4af677228c25e5b3308a5258a5d091
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-23 00:30:06 +0100

    add some methods

--- HashMap/src/Maps.java
+++ HashMap/src/Maps.java
@@ -9,6 +9,21 @@ public class Maps {
 		empIds.put("Anna-Marie", 444);
 		
 		System.out.println(empIds);
+		
+		System.out.println(empIds.get("Levan"));
+		
+		System.out.println(empIds.containsKey("Levan"));
+		
+		System.out.println(empIds.containsValue(333));
+		
+		empIds.put("Levan", 999);
+		System.out.println(empIds); //gadaawera
+		
+		empIds.replace("Levan", 979797);
+		System.out.println(empIds);
+		
+		empIds.putIfAbsent("Marie", 333777);
+		System.out.println(empIds);
 	}
 
 }
