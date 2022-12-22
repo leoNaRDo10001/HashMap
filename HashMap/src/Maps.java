ace4471b719e5696a357509620fd77b3935c013f
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-22 22:13:27 +0100

    first

---
+++ HashMap/src/Maps.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Maps {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> empIds = new HashMap<>();
+		
+		empIds.put("Levan", 333);
+		empIds.put("Anna-Marie", 444);
+		
+		System.out.println(empIds);
+	}
+
+}
